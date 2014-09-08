535a7a11ebba51fbc9d9430e05be9d4740f5f503
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-08 13:43:30 -0400

    updated to install 1.5

--- install.js
+++ install.js
@@ -2,25 +2,31 @@
 
 var path = require('path'),
 	fs = require('fs-extra'),
+	skipconffile = typeof process.env.npm_config_skip_install_periodic_ext ==='string',
 	Extensions = require('periodicjs.core.extensions'),
-	ExtensionCore = new Extensions({
-		extensionFilePath: path.resolve(__dirname,'../../content/extensions/extensions.json') 
-	}),
+	ExtensionCore = (skipconffile) ? 
+		new Extensions({
+			skipconffile:skipconffile,
+		}) : 
+		new Extensions({
+			extensionFilePath: path.resolve(__dirname,'../../content/extensions/extensions.json') 
+		}),
 	packagejsonFileJSON = fs.readJSONSync(path.resolve('./package.json')),
 	extname = packagejsonFileJSON.name,
 	extdir = path.resolve( './public'),
-	configdir = path.resolve( './config'),
 	extpublicdir = path.resolve(__dirname,'../../public/extensions/', extname),
-	extconfigdir = path.resolve(__dirname,'../../content/config/extensions/', extname),
 	extpackfile = path.resolve('./package.json'),
 	extconffile = path.resolve('./periodicjs.ext.json');
 
+// console.log('skipconffile',skipconffile);
+// $ npm install --skip-install-periodic-ext
+
 ExtensionCore.install({
+		enabled:true,
 		extname:extname,
 		extdir:extdir,
-		configdir:configdir,
+		skipconffile:skipconffile,
 		extpublicdir:extpublicdir,
-		extconfigdir:extconfigdir,
 		extpackfile:extpackfile,
 		extconffile:extconffile
 	},
