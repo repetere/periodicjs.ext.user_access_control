490e985c9087dabd8b908cc6a0ef62cc5972184e
Richard Shyong <rshyong@gmail.com>
2017-10-12 14:31:09 -0400

    done with User Access Control views

--- transforms/index.js
+++ transforms/index.js
@@ -2,13 +2,6 @@
 const periodic = require('periodicjs');
 const logger = periodic.logger;
 
-function testPreTransform(req) {
-  return new Promise((resolve, reject) => {
-    periodic.logger.silly('sample pre transfrom', req.params.id);
-    resolve(req);
-  });
-}
-
 /**
  * This function cleans up the roles object to show only the role userroleid and description.
  * @param {Object} req Express req object.
@@ -73,6 +66,22 @@ function showRolesInTextArea(req) {
 }
 
 /**
+ * This function cleans up the privileges object to show only the privilege userprivilegeid in the textarea field for the put request.
+ * @param {Object} req Express req object.
+ */
+function showPrivilegesInTextArea(req) {
+  return new Promise((resolve, reject) => {
+    let privileges = req.controllerData.standard_userrole.privileges;
+    if (privileges.length) {
+      privileges.forEach((privilege, i) => {
+        privileges[i] = privilege.userprivilegeid;
+      });
+    }
+    resolve(req);
+  })
+}
+
+/**
  * This function modifies the roles object to show only the mongoose id in the userrole property for user/account.
  * @param {Object} req Express req object.
  */
@@ -93,7 +102,7 @@ function modifyRoles(req) {
           let dne = JSON.parse(req.body.genericdocjson).filter(id => {
             return checkUserroles[id] === undefined;
           })
-          reject(`Userroleid(s) ${dne} do not exist`);
+          reject(`Userroleid(s) ${dne} does not exist`);
         } 
       })
       .then(() => {
@@ -114,23 +123,63 @@ function modifyRoles(req) {
   })
 }
 
+/**
+ * This function modifies the privileges object to show only the mongoose id in the privilege property for userroles.
+ * @param {Object} req Express req object.
+ */
+function modifyPrivileges(req) {
+  return new Promise ((resolve, reject) => {
+    let privileges = [];
+    let checkPrivileges = {};
+    periodic.datas.get('standard_userprivilege').query({ query: { userprivilegeid: { $in : JSON.parse(req.body.genericdocjson), }, } })
+      .then(result => {
+        result.forEach(privilege => {
+          privileges.push(privilege._id);
+          checkPrivileges[privilege.userprivilegeid] = 1;
+        })
+        if (privileges.length !== JSON.parse(req.body.genericdocjson).length) {
+          let dne = JSON.parse(req.body.genericdocjson).filter(id => {
+            return checkPrivileges[id] === undefined;
+          })
+          reject(`Userprivilegeid(s) ${dne} does not exist`);
+        } 
+      })
+      .then(() => {
+        return periodic.datas.get('standard_userrole').update({ id: req.params.id, updatedoc: { privileges, }, depopulate: false, })
+                .catch(err => {
+                  logger.error('Unable to update:', err)
+                  reject(err);
+                });
+      })
+      .then(() => {
+        req.redirectpath = req.originalUrl;
+        resolve(req);
+      })
+      .catch(err => {
+        logger.error('Unable to query for userprivilegeid', err);
+        reject(err);
+      });
+  })
+}
+
 module.exports = {
   pre: {
     GET: {
-      '/some/route/path/:id':[testPreTransform]
     },
     PUT: {
       '/b-admin/ext/user_access_control/standard_users/:id':[modifyRoles],
       '/b-admin/ext/user_access_control/standard_accounts/:id':[modifyRoles],
+      '/b-admin/ext/user_access_control/standard_userroles/:id':[modifyPrivileges]
     }
   },
   post: {
     GET: {
       '/b-admin/ext/user_access_control/standard_users':[showRoles],
-      '/b-admin/ext/user_access_control/standard_accounts':[showRoles],
       '/b-admin/ext/user_access_control/standard_users/:id':[showRolesInTextArea],
+      '/b-admin/ext/user_access_control/standard_accounts':[showRoles],
       '/b-admin/ext/user_access_control/standard_accounts/:id':[showRolesInTextArea],
-      '/b-admin/ext/user_access_control/standard_userroles':[showPrivileges]
+      '/b-admin/ext/user_access_control/standard_userroles':[showPrivileges],
+      '/b-admin/ext/user_access_control/standard_userroles/:id':[showPrivilegesInTextArea]
     },
     PUT: {
     }
